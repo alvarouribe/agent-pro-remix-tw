e3c364546dbe20b74f9f2adce90d16378e5dce40
Alvaro Uribe <iam@alvarouribe.cl>
2024-10-30 16:21:25 +1300

    Added static file and login screen

---
+++ app/routes/login.tsx
@@ -0,0 +1,27 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Agent Pro Remix Tailwind App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <section className="w-1/2 h-screen">
+        <h1>Login</h1>
+      </section>
+      <section className="w-1/2 h-screen border flex flex-col items-center justify-center text-center gap-2">
+        <img
+          src="/assets/house-login.png"
+          alt="Real Estate House"
+          className="block w-1/2"
+        />
+        <h2 className="text-2xl">Get your name out there</h2>
+        <p className="text-base leading-normal">Be seen by 1.1 million property<br/>seekers a month</p>
+      </section>
+    </div>
+  );
+}
